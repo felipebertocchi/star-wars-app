2a4c4126bfdc6a466eab769291e8f845e4a1636c
Felipe Bertocchi <fabertocchi@gmail.com>
2021-12-20 15:31:29 -0300

    set new routes for user login, register and favorite

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const path = require('path'); 
 
-
 const app = express();
 
 // SETTINGS
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 8080);
 
 // ROUTES
 const apiRoute = '/v1'
 const people = require('./routes/people');
 const planets = require('./routes/planets');
 const films = require('./routes/films');
+const signup = require('./routes/signup');
+const login = require('./routes/login');
+const favorite = require('./routes/favorite');
 
 // MIDDLEWARE
 app.use(apiRoute, people);
 app.use(apiRoute, planets);
 app.use(apiRoute, films);
+app.use(apiRoute, signup);
+app.use(apiRoute, login);
+app.use(apiRoute, favorite);
 
 // SERVER LISTENING
 app.listen(app.get('port'), () => {
