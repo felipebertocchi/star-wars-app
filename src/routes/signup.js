b3658df022889b676b185cb008af1a53f40bd64e
Felipe Bertocchi <fabertocchi@gmail.com>
2021-12-27 11:41:52 -0300

    User data validation with Joi

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,13 +1,22 @@
 const express = require('express');
-
 const router = express.Router();
 const User = require('../models/User');
+const Joi = require('joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(2).required(),
+    email: Joi.string().min(6).max(255).email().required(),
+    password: Joi.string().min(6).max(1024).required(),
+});
 
 router.post('/signup', async (req, res) => {
 
+    const {error} = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     // Chequear que no este registrado el mismo email en db
     const emailExists = await User.findOne({email: req.body.email})
-    if(emailExists) return res.status(400).send("Email has already been registered.");
+    if (emailExists) return res.status(400).send("Email has already been registered.");
 
 
     // Crear nuevo usuario
