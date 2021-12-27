fd85d063d7973899b63b6e97b6ebe037d85d7083
Felipe Bertocchi <fabertocchi@gmail.com>
2021-12-27 12:21:59 -0300

    Adds user login endpoint w/ validation

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,27 @@
 const express = require('express');
-
 const router = express.Router();
+const User = require('../models/User');
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
 
-router.post('/login', (req, res) => {
-})
+const schema = Joi.object({
+    email: Joi.string().min(6).max(255).email().required(),
+    password: Joi.string().min(6).max(1024).required(),
+});
 
+router.post('/login', async (req, res) => {
+
+    const {error} = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Chequear si el email si esta registrado
+    const user = await User.findOne({email: req.body.email})
+    if (!user) return res.status(400).send("Email is not registered.");
+
+    // Password match
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) return res.status(400).send("Invalid password.");
+
+    res.send("Logged in!");
+});
 module.exports = router;
