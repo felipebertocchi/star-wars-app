96aeda3afa627f59934c272e7c090dd0d30f3d98
Felipe Bertocchi <fabertocchi@gmail.com>
2021-12-28 20:54:36 -0300

    Changes category images

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,8 +3,8 @@ import Grid from '@material-ui/core/Grid'
 import routes from "../helpers/routes";
 
 export default function HomePage() {
-    const imageUrl1 = 'https://images.unsplash.com/photo-1513704519535-f5c81aa78d0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    const imageUrl2 = 'https://images.unsplash.com/photo-1627556592933-ffe99c1cd9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'
+    const imageUrl1 = 'https://www.diariovivo.com/wp-content/uploads/2019/11/starwars2.jpg'
+    const imageUrl2 = 'https://i.ytimg.com/vi/3lJeq573tVY/maxresdefault.jpg'
     const imageUrl3 = 'https://images.unsplash.com/photo-1518331539918-7a2dbf839306?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
     return (
         <div style={{width:'90%', margin:'0 auto'}}>
